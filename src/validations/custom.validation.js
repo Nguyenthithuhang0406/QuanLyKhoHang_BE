420fb769e30fe020a4d6d2875bdc03fdaa35a139
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-19 20:40:29 +0700

    🐛 fix (quan tâm ưu tiên route trong express)

--- src/validations/custom.validation.js
+++ src/validations/custom.validation.js
@@ -1,8 +1,8 @@
 const ObjectId = (value, helpers) => {
   if (!value.match(/^[0-9a-fA-F]{24}$/)) {
-    return helpers.message('ID Invalid ObjectId');
+    return helpers.message('"{{#label}} " ' + "must be a valid ObjectId.");
   }
   return value;
 };
 
-module.exports = {ObjectId};
+module.exports = { ObjectId };
