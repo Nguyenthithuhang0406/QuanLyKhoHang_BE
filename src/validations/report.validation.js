7807560ab64ecc967d1f20918fd4e86c5edcc73d
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-11-20 21:59:52 +0700

    .

--- src/validations/report.validation.js
+++ src/validations/report.validation.js
@@ -1,8 +1,8 @@
 const joi = require("joi");
 const reportExportImportInventory = {
   query: joi.object({
-    timeStart: joi.date().optional(),
-    timeEnd: joi.date().optional(),
+    timeStart: joi.date().optional().allow(""),
+    timeEnd: joi.date().optional().allow(""),
   }),
 };
 
